2b3bb450ed4f6139c02a9503fb907cb570e59e6a
bipin <bipin@pop-os.localdomain>
2019-12-23 14:31:04 +0545

    Add the model import file

---
+++ model.py
@@ -0,0 +1,74 @@
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Conv2D, Flatten, AvgPool2D, BatchNormalization, Dropout, Activation, MaxPooling2D
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras import regularizers
+from tensorflow.keras.layers import LeakyReLU
+from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.layers import Activation, Convolution2D, MaxPooling2D, BatchNormalization, Flatten, Dense, Dropout, Conv2D,MaxPool2D, ZeroPadding2D
+
+
+def get_model():
+    model = Sequential()
+
+    model.add(Convolution2D(32, (3,3), padding='same', use_bias=False, input_shape=(96,96,1)))
+    model.add(LeakyReLU(alpha = 0.1))
+    model.add(BatchNormalization())
+
+    model.add(Convolution2D(32, (3,3), padding='same', use_bias=False))
+    model.add(LeakyReLU(alpha = 0.1))
+    model.add(BatchNormalization())
+    model.add(MaxPool2D(pool_size=(2, 2)))
+
+    model.add(Convolution2D(64, (3,3), padding='same', use_bias=False))
+    model.add(LeakyReLU(alpha = 0.1))
+    model.add(BatchNormalization())
+
+    model.add(Convolution2D(64, (3,3), padding='same', use_bias=False))
+    model.add(LeakyReLU(alpha = 0.1))
+    model.add(BatchNormalization())
+    model.add(MaxPool2D(pool_size=(2, 2)))
+
+    model.add(Convolution2D(96, (3,3), padding='same', use_bias=False))
+    model.add(LeakyReLU(alpha = 0.1))
+    model.add(BatchNormalization())
+
+    model.add(Convolution2D(96, (3,3), padding='same', use_bias=False))
+    model.add(LeakyReLU(alpha = 0.1))
+    model.add(BatchNormalization())
+    model.add(MaxPool2D(pool_size=(2, 2)))
+
+    model.add(Convolution2D(128, (3,3),padding='same', use_bias=False))
+    # model.add(BatchNormalization())
+    model.add(LeakyReLU(alpha = 0.1))
+    model.add(BatchNormalization())
+
+    model.add(Convolution2D(128, (3,3),padding='same', use_bias=False))
+    model.add(LeakyReLU(alpha = 0.1))
+    model.add(BatchNormalization())
+    model.add(MaxPool2D(pool_size=(2, 2)))
+
+    model.add(Convolution2D(256, (3,3),padding='same',use_bias=False))
+    model.add(LeakyReLU(alpha = 0.1))
+    model.add(BatchNormalization())
+
+    model.add(Convolution2D(256, (3,3),padding='same',use_bias=False))
+    model.add(LeakyReLU(alpha = 0.1))
+    model.add(BatchNormalization())
+    model.add(MaxPool2D(pool_size=(2, 2)))
+
+    model.add(Convolution2D(512, (3,3), padding='same', use_bias=False))
+    model.add(LeakyReLU(alpha = 0.1))
+    model.add(BatchNormalization())
+
+    model.add(Convolution2D(512, (3,3), padding='same', use_bias=False))
+    model.add(LeakyReLU(alpha = 0.1))
+    model.add(BatchNormalization())
+
+
+    model.add(Flatten())
+    model.add(Dense(512,activation='relu'))
+    model.add(Dropout(0.1))
+    model.add(Dense(30))
+    model.summary()
+
+    return model
